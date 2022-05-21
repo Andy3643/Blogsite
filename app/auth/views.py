d6754c47d1dc6a17b8ba4104bd499f49c3ef1442
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-05-21 09:58:26 +0300

    ft:add auth route to redirect user to login page and add new user to  database.

--- app/auth/views.py
+++ app/auth/views.py
@@ -7,6 +7,21 @@ from .forms import LoginForm, RegistrationForm
 from ..models import User
 
 
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to KalebsBlog","email/welcome_user",user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)    
+
+
 @auth.route('/login',methods=['GET','POST'])
 def login():
     login_form = LoginForm()
